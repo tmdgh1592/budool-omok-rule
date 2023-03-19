fc4566f5c04406b9124b75c8158aec3327391cc5
tmdgh1592 <tmdgh1592@naver.com>
2023-03-19 21:32:45 +0900

    refactor: add function to return all directions

--- src/main/kotlin/rule/wrapper/direction/Directions.kt
+++ src/main/kotlin/rule/wrapper/direction/Directions.kt
@@ -1,17 +1,12 @@
 package rule.wrapper.direction
 
-import rule.*
+import rule.Col
+import rule.Direction
+import rule.Row
 import rule.other.Iterator
 
 class Directions {
-    private val directions = listOf(
-        TOP_DIRECTION, BOTTOM_DIRECTION,
-        LEFT_DIRECTION, RIGHT_DIRECTION,
-        LEFT_BOTTOM_DIRECTION, RIGHT_TOP_DIRECTION,
-        LEFT_TOP_DIRECTION, RIGHT_BOTTOM_DIRECTION,
-    )
-
-    fun iterator(): Iterator<Direction<Row, Col>> = DirectionsIterator(directions)
+    fun iterator(): Iterator<Direction<Row, Col>> = DirectionsIterator(all())
 
     companion object {
         private val TOP_DIRECTION = Direction(1, 0)
@@ -22,5 +17,12 @@ class Directions {
         private val RIGHT_TOP_DIRECTION = Direction(1, 1)
         private val LEFT_TOP_DIRECTION = Direction(1, -1)
         private val RIGHT_BOTTOM_DIRECTION = Direction(-1, 1)
+
+        private fun all(): List<Direction<Row, Col>> = listOf(
+            TOP_DIRECTION, BOTTOM_DIRECTION,
+            LEFT_DIRECTION, RIGHT_DIRECTION,
+            LEFT_BOTTOM_DIRECTION, RIGHT_TOP_DIRECTION,
+            LEFT_TOP_DIRECTION, RIGHT_BOTTOM_DIRECTION,
+        )
     }
 }
