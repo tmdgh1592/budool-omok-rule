dc4c5d24a24c31fd6ec10ee0091d8d1419df81c2
tmdgh1592 <tmdgh1592@naver.com>
2023-03-19 15:59:58 +0900

    refactor: modify to keep line formatting and conventions
    - Remove unnecessary comments.
    - Write a comma at the end of the function argument.

--- src/main/kotlin/rule/RenjuRule.kt
+++ src/main/kotlin/rule/RenjuRule.kt
@@ -4,19 +4,20 @@ class RenjuRule(
     boardWidth: Int = DEFAULT_BOARD_WIDTH,
     boardHeight: Int = DEFAULT_BOARD_HEIGHT,
 ) : OmokRule(boardWidth, boardHeight) {
+
     override fun checkThreeToThreePoint(
         blackPositions: List<Position<Row, Col>>,
         whitePositions: List<Position<Row, Col>>,
         startPosition: Position<Row, Col>,
     ): KoRule = checkAllThreeToThreeDirections(blackPositions, whitePositions, startPosition)
 
+
     override fun checkFourToFourPoint(
         blackPositions: List<Position<Row, Col>>,
         whitePositions: List<Position<Row, Col>>,
         startPosition: Position<Row, Col>,
-    ): KoRule {
-        return checkAllFourToFourDirections(blackPositions, whitePositions, startPosition)
-    }
+    ): KoRule = checkAllFourToFourDirections(blackPositions, whitePositions, startPosition)
+
 
     override fun checkOverline(
         stonesPositions: List<Position<Row, Col>>,
@@ -25,41 +26,32 @@ class RenjuRule(
         for (moveDirection in directions) {
             val forwardCount = findLongOmok(stonesPositions, startPosition, moveDirection, FORWARD_WEIGHT)
             val backCount = findLongOmok(stonesPositions, startPosition, moveDirection, BACK_WEIGHT)
-
             if (forwardCount + backCount > MAX_DISTANCE_TWO_BLOCKED_WHITE_STONE) return KoRule.KO_OVERLINE
         }
         return KoRule.NOT_KO
     }
+
 
     private fun checkAllThreeToThreeDirections(
         blackPositions: List<Position<Row, Col>>,
         whitePositions: List<Position<Row, Col>>,
         startPosition: Position<Row, Col>,
     ): KoRule {
-        var threeCount = DEFAULT_SAME_STONE_COUNT
+        var threeCount = 0
 
         for (moveDirection in directions) {
             val (forwardCount, forwardEmptyCount) = findStraight(
-                blackPositions,
-                whitePositions,
-                startPosition,
-                moveDirection,
-                FORWARD_WEIGHT,
-                THREE_TO_THREE_SIZE,
+                blackPositions, whitePositions,
+                startPosition, moveDirection,
+                FORWARD_WEIGHT, THREE_TO_THREE_SIZE,
             )
             val (backCount, backEmptyCount) = findStraight(
-                blackPositions,
-                whitePositions,
-                startPosition,
-                moveDirection,
-                BACK_WEIGHT,
-                THREE_TO_THREE_SIZE,
+                blackPositions, whitePositions,
+                startPosition, moveDirection,
+                BACK_WEIGHT, THREE_TO_THREE_SIZE,
             )
 
-            // 만약 빈 칸이 2 미만이고, 같은 돌 개수가 무조건 3이면 3-3 가능성 ok
             if (forwardCount + backCount - 1 == THREE_TO_THREE_SIZE && forwardEmptyCount + backEmptyCount <= MAX_EMPTY_SIZE) {
-                // 백돌 양쪽 합 6칸 이내에 2개 이상 있는지 확인한다.
-                // 닫혀 있으면 다른 방향 확인
                 if (!isBlockedByWhiteStoneInSix(whitePositions, startPosition, moveDirection)) threeCount++
                 if (threeCount == FOUL_CONDITION_SIZE) return KoRule.KO_THREE_TO_THREE
             }
@@ -67,34 +59,28 @@ class RenjuRule(
         return KoRule.NOT_KO
     }
 
+
     private fun checkAllFourToFourDirections(
         blackPositions: List<Position<Row, Col>>,
         whitePositions: List<Position<Row, Col>>,
         startPosition: Position<Row, Col>,
     ): KoRule {
-        var fourCount = DEFAULT_SAME_STONE_COUNT
+        var fourCount = 0
+
         for (moveDirection in directions) {
             val (forwardCount, forwardEmptyCount) = findStraight(
-                blackPositions,
-                whitePositions,
-                startPosition,
-                moveDirection,
-                FORWARD_WEIGHT,
-                FOUR_TO_FOUR_SIZE,
+                blackPositions, whitePositions,
+                startPosition, moveDirection,
+                FORWARD_WEIGHT, FOUR_TO_FOUR_SIZE,
             )
             val (backCount, backEmptyCount) = findStraight(
-                blackPositions,
-                whitePositions,
-                startPosition,
-                moveDirection,
-                BACK_WEIGHT,
-                FOUR_TO_FOUR_SIZE,
+                blackPositions, whitePositions,
+                startPosition, moveDirection,
+                BACK_WEIGHT, FOUR_TO_FOUR_SIZE,
             )
-            // 만약 빈 칸이 2 미만이고, 같은 돌 개수가 무조건 4이면 4-4 가능성 ok
+
             val stoneCount = forwardCount + backCount - 1
-            // 1자 4-4
             if (stoneCount >= 5 && forwardEmptyCount == 1 && backEmptyCount == 1) return KoRule.KO_FOUR_TO_FOUR
-            // 각각 다른 방향 4-4
             if (stoneCount == FOUR_TO_FOUR_SIZE && forwardEmptyCount + backEmptyCount <= MAX_EMPTY_SIZE) {
                 fourCount++
                 if (fourCount == FOUL_CONDITION_SIZE) return KoRule.KO_FOUR_TO_FOUR
@@ -103,27 +89,25 @@ class RenjuRule(
         return KoRule.NOT_KO
     }
 
+
     private fun isBlockedByWhiteStoneInSix(
         whitePositions: List<Position<Row, Col>>,
         position: Position<Row, Col>,
         direction: Direction<Row, Col>,
     ): Boolean {
         val (oneDirMoveCount, oneDirFound) = checkWhite(
-            whitePositions,
-            position,
-            direction,
-            FORWARD_WEIGHT,
+            whitePositions, position,
+            direction, FORWARD_WEIGHT,
         )
         val (otherDirMoveCount, otherDirFound) = checkWhite(
-            whitePositions,
-            position,
-            direction,
-            BACK_WEIGHT,
+            whitePositions, position,
+            direction, BACK_WEIGHT,
         )
-        // 양 방향 6칸 이하에 각각 1개씩 있으면 참
+
         return oneDirMoveCount + otherDirMoveCount <= MAX_DISTANCE_TWO_BLOCKED_WHITE_STONE &&
                 oneDirFound && otherDirFound
     }
+
 
     private fun checkWhite(
         whiteStones: List<Position<Row, Col>>,
@@ -132,7 +116,8 @@ class RenjuRule(
         weight: MoveWeight,
     ): Pair<Int, Boolean> {
         var (curRow, curCol) = Pair(
-            position.first + direction.first * weight, position.second + direction.second * weight
+            position.first + direction.first * weight,
+            position.second + direction.second * weight
         )
         var moveCount = 0
         while (inRange(curRow, curCol) && moveCount <= MAX_DISTANCE_TWO_BLOCKED_WHITE_STONE) {
@@ -143,6 +128,7 @@ class RenjuRule(
         }
         return Pair(moveCount, false)
     }
+
 
     private fun findStraight(
         blackPositions: List<Position<Row, Col>>,
@@ -157,10 +143,10 @@ class RenjuRule(
         var emptyCount = DEFAULT_EMPTY_COUNT
         var (currentRow, currentCol) = Pair(startRow + direction.first * weight, startCol + direction.second * weight)
 
-        while (inRange(
-                currentRow,
-                currentCol
-        ) && emptyCount <= MAX_EMPTY_SIZE && sameStoneCount < stoneCount && !whitePositions.isPlaced(currentRow, currentCol)
+        while (inRange(currentRow, currentCol) &&
+            !whitePositions.isPlaced(currentRow, currentCol) &&
+            emptyCount <= MAX_EMPTY_SIZE &&
+            sameStoneCount < stoneCount
         ) {
             val hasBlackStone = blackPositions.isPlaced(currentRow, currentCol)
             val hasWhiteStone = whitePositions.isPlaced(currentRow, currentCol)
@@ -180,6 +166,7 @@ class RenjuRule(
         return Pair(sameStoneCount, emptyCount)
     }
 
+
     private fun findLongOmok(
         stonesPositions: List<Position<Row, Col>>,
         startPosition: Position<Row, Col>,
@@ -197,6 +184,7 @@ class RenjuRule(
         }
         return sameStoneCount
     }
+
 
     companion object {
         private val RIGHT_DIRECTION = Pair(1, 0)
